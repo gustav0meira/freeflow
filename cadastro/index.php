b74f2483b312e4a6127b2511d766f9dae58c2f88
Gustavo <gusmeiradsg@gmail.com>
2023-05-29 18:44:05 -0300

    refactor(cadastro): bugs

--- cadastro/index.php
+++ cadastro/index.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Login | FreeFlow</title>
+	<title>Cadastro | FreeFlow</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 
 	<link rel="icon" type="image/x-icon" href="../assets/favicon.png">
